5beb91c5b468e7691dc9cb93ebec0e2bdc1c4c18
Karoline Carvalho Paiva Terceiro Lima <karolinepaivaterceiro@gmail.com>
2026-05-22 08:19:40 -0300

    segundo commit do JS

--- script.js
+++ script.js
@@ -29,3 +29,13 @@ function receberQuantidadePessoas(evento) {
 
     }
 }
+
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']");
+
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", receberPorcentagem);
+});
+
+function receberPorcentagem(evento) {
+    console.log(evento.target.value);
+}
